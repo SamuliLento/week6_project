85eb6f527a5c132332e5e9b0251499c18668ae35
SamuliLento <samuli.lento@gmail.com>
2022-10-13 10:51:55 +0000

    Update 14 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,131 @@
-import "./styles.css";
+if (document.readyState !== "loading") {
+  console.log("Document is ready!");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is ready after waiting!");
+    initializeCode();
+  });
+}
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function initializeCode() {
+  const jsonQuery = {
+    query: [
+      {
+        code: "Vuosi",
+        selection: {
+          filter: "item",
+          values: [
+            "2000",
+            "2001",
+            "2002",
+            "2003",
+            "2004",
+            "2005",
+            "2006",
+            "2007",
+            "2008",
+            "2009",
+            "2010",
+            "2011",
+            "2012",
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2017",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+          ],
+        },
+      },
+      {
+        code: "Alue",
+        selection: {
+          filter: "item",
+          values: ["SSS"],
+        },
+      },
+      {
+        code: "Tiedot",
+        selection: {
+          filter: "item",
+          values: ["vaesto"],
+        },
+      },
+    ],
+    response: {
+      format: "json-stat2",
+    },
+  };
+
+  const getData = async () => {
+    const url =
+      "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px";
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(jsonQuery),
+    });
+    if (!res.ok) {
+      return;
+    }
+    const data = await res.json();
+
+    return data;
+  };
+
+  const buildChart = async () => {
+    const data = await getData();
+    console.log(data);
+
+    const values = data.value;
+    console.log(values);
+
+    const population = {
+      name: "Population",
+      values: values,
+    };
+
+    const chartData = {
+      labels: [
+        "2000",
+        "2001",
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017",
+        "2018",
+        "2019",
+        "2020",
+        "2021",
+      ],
+      datasets: population,
+    };
+
+    const chart = new frappe.Chart("#chart", {
+      title: "Population growth in Finland",
+      data: chartData,
+      type: "line",
+      height: 450,
+      colors: ["#eb5146"],
+    });
+  };
+
+  buildChart();
+}
